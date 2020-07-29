4c0105ddccee853801cab81bb43fcfd4378d9957
Devin Starks <devstarks@gmail.com>
2020-07-29 08:10:52 -0400

    app working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { withFirebase } from './services/Firebase';
-import generateUid from './services/uid'
+import generateUid from './utils/uid'
 import Cookies from 'js-cookie'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-  componentDidMount() {
-    const firebase = this.props.firebase;
-    const uid = generateUid()
+    this.state = {
+      loading: true,
+      activeUserCount: null,
+      uid: null
+    };
+  }
 
-    var userStatusDatabaseRef = firebase.database.ref('/online/' + uid);
-    debugger
+  async componentDidMount() {
+    await this.setCookie();
+
+    this.updateActiveUserCount();
+    this.subscribeToActiveUserCount();
+  }
+
   setCookie() {
     if(!Cookies.get('uid')){
       Cookies.set('uid', generateUid());
@@ -20,21 +29,54 @@ class App extends React.Component {
     this.setState({ uid: Cookies.get('uid') });
   }
 
-    firebase.database.ref('.info/connected').on('value', (snapshot) => {
+  updateActiveUserCount() {
+    const firebase = this.props.firebase;
+    const userStatusDatabaseRef = firebase.database.ref('/online/' + this.state.uid);
+    const dbRef = firebase.database.ref('.info/connected');
+
+    dbRef.on('value', (snapshot) => {
       // If we're not currently connected, don't do anything.
       if (snapshot.val() === false) return;
 
       userStatusDatabaseRef.onDisconnect()
-                           .set(null) //delete record once disconnected
+                           .set(false)
                            .then(() => userStatusDatabaseRef.set(true));
     });
+  }
+
+  subscribeToActiveUserCount() {
+    const firebase = this.props.firebase;
+    const onlineUsersRef = firebase.database.ref('/online');
+
+    onlineUsersRef.orderByValue().startAt(true).on('value', (snapshot) => {
+      this.setState({
+        activeUserCount: snapshot.numChildren(),
+        loading: false
+      })
+    });
+  }
+
+  renderUserCount() {
+    return (
+      <div className="App-content">
+        <h1>Welcome!</h1>
+        <h3>Including yours, this many browsers are currently accessing this app:</h3>
+        <span className="App-userCount">{this.state.activeUserCount}</span>
+      </div>
+    );
+  }
+
+  renderLoading() {
+    return (
+      <p>Loading...</p>
+    );
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-container">
-          <p></p>
+          {this.state.loading ? this.renderLoading() : this.renderUserCount()}
         </div>
       </div>
     );
